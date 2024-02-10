4243d6c5c689e528fff8b48746c658176914f027
jonghyunhub <chuwhdgus@gmail.com>
2024-02-10 16:35:04 +0900

    config : application.yml feign 설정 추가

--- src/main/java/com/demo/feign/feign/client/DemoFeignClient.java
+++ src/main/java/com/demo/feign/feign/client/DemoFeignClient.java
@@ -1,7 +1,12 @@
 package com.demo.feign.feign.client;
 
+import com.demo.feign.common.dto.BaseResponseInfo;
 import com.demo.feign.feign.config.DemoFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "demo-client",
@@ -10,5 +15,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 )
 public interface DemoFeignClient {
 
-
+    @GetMapping("/get") // ->  http://localhost:8080/target_server/get
+    ResponseEntity<BaseResponseInfo> callGet(
+            @RequestHeader("CustomHeaderName") String customHeaderName,
+            @RequestParam("name") String name,
+            @RequestParam("age") Long age
+    );
 }
--- src/main/java/com/demo/feign/service/DemonService.java
+++ src/main/java/com/demo/feign/service/DemonService.java
@@ -1,13 +1,30 @@
 package com.demo.feign.service;
 
+import com.demo.feign.common.dto.BaseResponseInfo;
+import com.demo.feign.feign.client.DemoFeignClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DemonService {
 
+    private final DemoFeignClient demoFeignClient;
+
     public String get() {
+        ResponseEntity<BaseResponseInfo> response = demoFeignClient.callGet(
+                "CustomHeader",
+                "CustomName",
+                1L
+        );
+
+        log.info("Name : {}", response.getBody().getName());
+        log.info("Age : {}", response.getBody().getAge());
+        log.info("Header : {}", response.getBody().getHeader());
+
         return "get";
     }
 
