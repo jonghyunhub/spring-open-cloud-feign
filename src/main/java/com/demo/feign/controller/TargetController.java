cd29f9c219740dd1cc6566c43139136310c6b232
jonghyunhub <chuwhdgus@gmail.com>
2024-02-10 16:56:15 +0900

    feat : 학습용 feign 메서드 추가

--- src/main/java/com/demo/feign/controller/TargetController.java
+++ src/main/java/com/demo/feign/controller/TargetController.java
@@ -1,12 +1,22 @@
 package com.demo.feign.controller;
 
+import com.demo.feign.common.dto.BaseResponseInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/target_server")
 public class TargetController {
 
+    @GetMapping("/get")
+    public BaseResponseInfo demoGet(@RequestHeader("CustomHeaderName") String header,
+                                    @RequestParam("name") String name,
+                                    @RequestParam("age") Long age) {
+        return BaseResponseInfo.builder()
+                .header(header)
+                .name(name )
+                .age(age)
+                .build();
+    }
 }
